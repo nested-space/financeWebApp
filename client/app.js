8d112ec144fae914d1ff1fcc30af023675de16b8
nestedspace <ed.edenrump@gmail.com>
2019-10-18 03:27:04 -0400

    intital separation of client and server. untested

---
+++ client/app.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env nodemon
+
+//Node Modules
+const express         = require('express');
+const app = express();
+
+//Routes
+const www_finance = require('./routes/www/finance');
+
+//Express settings
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use('/finance/', www_finance);
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('*', function(req, res){
+    res.render('404');
+});
+
+const port = 3001;
+app.listen(port, process.env.IP, function(){
+    console.log(`Serving on port ${port}`);
+});
